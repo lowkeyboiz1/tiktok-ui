48b85bc9c12d0cbd7b75627ef9846b873a55ffd0
unknown <khangluong2002@gmail.com>
2023-04-04 23:21:53 +0700

    4.4.2023 Header search item

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/856d6e040a2b3181553f302cdef7f5f5~c5_300x300.webp?x-expires=1680796800&x-signature=GzrPFLbJEKDtXXP2knZlbuM%2Fm18%3D"
+                alt="hoa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
-import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
-import images from '~/assets/images';
+import 'tippy.js/dist/tippy.css'; // optional
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react';
-import 'tippy.js/dist/tippy.css'; // optional
+
+import images from '~/assets/images';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
 const cx = classNames.bind(styles);
 
 function Header() {
@@ -14,7 +17,7 @@ function Header() {
     useEffect(() => {
         setTimeout(() => {
             setSearchResult([1, 2, 3]);
-        }, 3000);
+        }, 0);
     }, []);
 
     return (
@@ -26,8 +29,14 @@ function Header() {
                     interactive={true}
                     visible={searchResult.length > 0}
                     render={(attrs) => (
-                        <div className="box" tabIndex="-1" {...attrs}>
-                            My tippy box
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
                         </div>
                     )}
                 >
