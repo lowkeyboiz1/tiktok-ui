aea4646a94b432267310553c38ce3a193023c3ea
unknown <khangluong2002@gmail.com>
2023-04-12 16:44:38 +0700

    logic header tiktok #1

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,7 @@
 const images = {
     logo: require('~/assets/images/logo.svg').default,
     noImage: require('~/assets/images/no-image.png'),
-    message: require('~/assets/images/message.svg').default,
+    avatarUser: require('~/assets/images/avatarUser.png'),
 };
 
 export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,30 +1,21 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css'; // optional
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faCircleXmark,
-    faSpinner,
-    faPlus,
-    faEllipsisVertical,
-    faLanguage,
-    faGear,
-    faSignOut,
-} from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faEllipsisVertical, faLanguage, faGear, faSignOut } from '@fortawesome/free-solid-svg-icons';
 
 import images from '~/assets/images';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-import { Wrapper as PopperWrapper } from '~/components/Popper';
-import AccountItem from '~/components/AccountItem';
+
 import Button from '~/components/Button';
 import Menu from '~/components/Popper/Menu';
 
 import { faUser, faCircleQuestion, faKeyboard, faMoon } from '@fortawesome/free-regular-svg-icons';
 import { faTiktok } from '@fortawesome/free-brands-svg-icons';
-import { InboxIcon, MessageIcon, SearchIcon } from '~/components/Icons';
+import { InboxIcon, MessageIcon } from '~/components/Icons';
 import Image from '~/components/Image';
+import Search from '../Search';
 
 const cx = classNames.bind(styles);
 
@@ -93,14 +84,6 @@ const userMenu = [
 ];
 
 function Header() {
-    const [searchResult, setSearchResult] = useState([]);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([1, 2, 3]);
-        }, 3000);
-    }, []);
-
     //handle logic
     const handleMenuChange = (menuItem) => {
         console.log(menuItem);
@@ -110,35 +93,7 @@ function Header() {
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <img src={images.logo} alt="tiktok" />
-
-                <Tippy
-                    interactive
-                    offset={[10, 10]}
-                    visible={searchResult.length > 1}
-                    render={(attrs) => (
-                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                            <PopperWrapper>
-                                <h4 className={cx('search-title')}>Accounts</h4>
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                            </PopperWrapper>
-                        </div>
-                    )}
-                >
-                    <div className={cx('search')}>
-                        <input placeholder="Search accounts and videos" spellCheck={false} />
-                        <button className={cx('clear')}>
-                            <FontAwesomeIcon icon={faCircleXmark} />
-                        </button>
-                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                        <button className={cx('btn-search')}>
-                            <SearchIcon />
-                        </button>
-                    </div>
-                </Tippy>
-
+                <Search />
                 <div className={cx('actions')}>
                     {currentUser ? (
                         <>
@@ -171,11 +126,7 @@ function Header() {
 
                     <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
                         {currentUser ? (
-                            <Image
-                                className={cx('user-avatar')}
-                                src="https://files.fullstack.edu.vn/f8-prod/user_avatars/1/623d4b2d95cec.png"
-                                alt="Nguyen Van A"
-                            />
+                            <Image className={cx('user-avatar')} src={images.avatarUser} alt="K" />
                         ) : (
                             <button className={cx('more-btn')}>
                                 <FontAwesomeIcon icon={faEllipsisVertical} />
