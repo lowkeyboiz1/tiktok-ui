f33c177fcfeaafdd99108408a07cd623669e293c
unknown <khangluong2002@gmail.com>
2023-04-16 13:08:50 +0700

    navbar ui tiktok #1

---
+++ src/components/SuggestedAccounts/SuggestedAccounts.js
@@ -0,0 +1,21 @@
+import classNames from 'classnames/bind';
+import styles from './SuggestedAccounts.scss';
+import SuggestedAccountItem from './SuggestedAccountItem';
+
+const cx = classNames.bind(styles);
+
+function SuggestedAccounts({ label }) {
+    return (
+        <div className={cx('wrapper')}>
+            <p className={cx('label')}>{label}</p>
+            <SuggestedAccountItem />
+            <SuggestedAccountItem />
+            <SuggestedAccountItem />
+            <SuggestedAccountItem />
+            <SuggestedAccountItem />
+            <div className={cx('more-btn')}>See all</div>
+        </div>
+    );
+}
+
+export default SuggestedAccounts;
